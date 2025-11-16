6df1258877534009208d222d7365fd6f19039f3f
Vladimir Nehoc <vladimir.n@viletech.pro>
2025-11-16 20:38:35 +0400

    fix templates path

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -13,7 +13,7 @@ import { MailService } from "./mail.service";
       useFactory: (config: ConfigService) => {
         const templatesDir =
           process.env.NODE_ENV === "production"
-            ? join(__dirname, "templates")
+            ? join(process.cwd(), "dist", "mail", "templates")
             : join(process.cwd(), "src", "mail", "templates");
 
         return {
